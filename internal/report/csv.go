8dad01796a37dd7dddd69d277e0e27468d174045
Bailey <bailey.butler.234@gmail.com>
2024-09-15 15:41:36 +1000

    csv

--- internal/report/csv.go
+++ internal/report/csv.go
@@ -1,12 +1,79 @@
 package report
 
-import "time"
+import (
+	"bytes"
+	"fmt"
+	"time"
+
+	"github.com/baely/officetracker/pkg/model"
+)
+
+type csvLine struct {
+	Date  string
+	State string
+}
 
 func (r *fileReporter) GenerateCSV(userID int, start, end time.Time) ([]byte, error) {
-	_, err := r.Generate(userID, start, end)
+	report, err := r.Generate(userID, start, end)
 	if err != nil {
+		err = fmt.Errorf("failed to generate report: %w", err)
 		return nil, err
 	}
 
-	return nil, nil
+	days := getDays(start, end)
+
+	var lines []csvLine
+
+	for _, day := range days {
+		monthData := report.Get(day.Month(), day.Year())
+		state := monthData.Days[day.Day()].State
+		lines = append(lines, csvLine{
+			Date:  day.Format("2006-01-02"),
+			State: getState(state),
+		})
+	}
+
+	return buildCsv(lines), nil
+}
+
+func getDays(start, end time.Time) []time.Time {
+	var days []time.Time
+	start = time.Date(start.Year(), start.Month(), start.Day(), 0, 0, 0, 0, start.Location())
+	for start.Before(end) {
+		// only if weekday
+		if start.Weekday() > 0 && start.Weekday() < 6 {
+			days = append(days, start)
+		}
+		start = start.AddDate(0, 0, 1)
+	}
+	return days
+}
+
+func getState(state model.State) string {
+	switch state {
+	case model.StateWorkFromHome:
+		return "Home"
+	case model.StateWorkFromOffice:
+		return "Office"
+	case model.StateOther:
+		fallthrough
+	case model.StateUntracked:
+		fallthrough
+	default:
+		return ""
+	}
+}
+
+func buildCsv(lines []csvLine) []byte {
+	buf := new(bytes.Buffer)
+
+	// Write header
+	buf.WriteString("Date,State\n")
+
+	// Write lines
+	for _, line := range lines {
+		buf.WriteString(fmt.Sprintf("%s,%s\n", line.Date, line.State))
+	}
+
+	return buf.Bytes()
 }
---
+++ internal/report/report.go
@@ -0,0 +1,86 @@
+package report
+
+import (
+	"fmt"
+	"path"
+	"time"
+
+	"github.com/google/uuid"
+
+	"github.com/baely/officetracker/internal/database"
+	"github.com/baely/officetracker/pkg/model"
+)
+
+type Key struct {
+	Month time.Month
+	Year  int
+}
+
+type Report struct {
+	Months map[Key]model.MonthState
+}
+
+func (r Report) Get(month time.Month, year int) model.MonthState {
+	key := Key{
+		Month: month,
+		Year:  year,
+	}
+	return r.Months[key]
+}
+
+type Reporter interface {
+	Generate(userID int, start, end time.Time) (Report, error)
+	GenerateCSV(userID int, start, end time.Time) ([]byte, error)
+	GeneratePDF(userID int, start, end time.Time) ([]byte, error)
+}
+
+type fileReporter struct {
+	db database.Databaser
+}
+
+func New(db database.Databaser) Reporter {
+	return &fileReporter{
+		db: db,
+	}
+}
+
+func (r *fileReporter) Generate(userID int, start, end time.Time) (Report, error) {
+	report := Report{
+		Months: make(map[Key]model.MonthState),
+	}
+	months := getMonths(start, end)
+
+	for _, month := range months {
+		key := Key{
+			Month: month.Month(),
+			Year:  month.Year(),
+		}
+		monthData, err := r.db.GetMonth(userID, int(month.Month()), month.Year())
+		if err != nil {
+			err = fmt.Errorf("failed to get month state: %w", err)
+			return Report{}, err
+		}
+		report.Months[key] = monthData
+	}
+
+	return report, nil
+}
+
+func getMonths(start, end time.Time) []time.Time {
+	var months []time.Time
+	start = time.Date(start.Year(), start.Month(), 1, 0, 0, 0, 0, start.Location())
+
+	for start.Before(end) {
+		months = append(months, start)
+		start = start.AddDate(0, 1, 0)
+	}
+
+	return months
+}
+
+// generateFilename generates a filename
+func generateFilename(base string, ext string) string {
+	id := uuid.NewString()
+	filename := id + "." + ext
+	return path.Join(base, filename)
+}
