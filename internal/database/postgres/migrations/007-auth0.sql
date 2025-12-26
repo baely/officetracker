8586cdb270929a307421beddd7a5e95f53efef0a
Bailey <bailey.butler.234@gmail.com>
2025-12-27 01:04:20 +1100

    tid bit more

--- internal/database/postgres/migrations/007-auth0.sql
+++ internal/database/postgres/migrations/007-auth0.sql
@@ -4,3 +4,15 @@ CREATE TABLE IF NOT EXISTS "auth0_users" (
     "user_id" INTEGER REFERENCES "users" (user_id),
     PRIMARY KEY ("sub")
 );
+
+INSERT INTO auth0_users (sub, profile, user_id)
+SELECT
+    'github|' || gh_id,
+    json_build_object(
+        'sub', 'github|' || gh_id,
+        'nickname', gh_user,
+        'picture', 'https://avatars.githubusercontent.com/u/' || gh_id || '?v=4'
+    )::text,
+    user_id
+FROM gh_users
+ON CONFLICT (sub) DO NOTHING;
